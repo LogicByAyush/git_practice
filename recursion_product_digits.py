7948650699f48cc4dad82f1e2f30283020b248af
Ayush Gupta <uhbayush@gmail.com>
2026-06-19 11:15:29 +0530

    Added by recursion_product_digits

---
+++ recursion_product_digits.py
@@ -0,0 +1,8 @@
+# recursion_product_digits.py
+def product_digit( num ):
+    if num == 0:
+        return 1
+    return num%10 * product_digit( num // 10 )
+
+number = int(input("Enter the number: "))
+print(f"product of all digit of {number} is {product_digit(number)}")
